8396a0a7b028754c2f819e2aea2e03b007196f3a
Rahul <rahulrrao@gmail.com>
2025-08-15 22:20:38 +0930

    Updated main.py for deploying on render 4

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def upload_image(request: Request, file: UploadFile = File(...)):
     try:
         # Send to Replicate for background removal + resize
         output_url = replicate.run(
-            "rembg:1.4.2",
+            "cjwbw/rembg:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003",
             input={"image": data_uri}
         )
     except Exception as e:
